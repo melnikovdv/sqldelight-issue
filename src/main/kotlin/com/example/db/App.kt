2fd22eb28ba58f570709702649604f316b4bc23f
mdv <melnikovdv@gmail.com>
2020-05-15 16:35:47 +0300

    workaround with field order

--- src/main/kotlin/com/example/db/App.kt
+++ src/main/kotlin/com/example/db/App.kt
@@ -7,6 +7,6 @@ fun main(args: Array<String>) {
     db.tableQueries.insert(MyTable.Impl(0, 1, 10))
     db.tableQueries.insert(MyTable.Impl(0, 2, 10))
     db.tableQueries.insert(MyTable.Impl(0, 3, 10))
-    val rows = db.tableQueries.select(setOf(1, 2), 10).executeAsList()
+    val rows = db.tableQueries.select(10, setOf(1, 2)).executeAsList()
     println(rows.toString())
 }
